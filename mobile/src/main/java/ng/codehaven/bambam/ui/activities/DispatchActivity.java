f80ac2d581fcdd97d86f1028fdd066c052a7cf5f
Thompson Edolo <verygreenboi@live.com>
2015-08-02 14:39:04 +0100

    Refactor DispatchActivity.java to use Intent utility

--- mobile/src/main/java/ng/codehaven/bambam/ui/activities/DispatchActivity.java
+++ mobile/src/main/java/ng/codehaven/bambam/ui/activities/DispatchActivity.java
@@ -6,17 +6,45 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.parse.ParseUser;
 
+import ng.codehaven.bambam.BuildType;
+import ng.codehaven.bambam.utils.Connectivity;
+import ng.codehaven.bambam.utils.IntentUtil;
+
 public class DispatchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Connectivity mConnect = new Connectivity(this);
+        IntentUtil iUtil = new IntentUtil(this);
         // Check if there is current user info
         if (ParseUser.getCurrentUser() != null) {
-            startActivity(new Intent(this, Home.class));
+            iUtil.goToActivity(new Intent(this, Home.class));
         } else {
-            // Start and intent for the logged out activity
-            startActivity(new Intent(this, SignIn.class));
+            if (!mConnect.isConnectingToInternet()) {
+
+                switch (BuildType.type) {
+                    case 0:
+                        doDebug();
+                        break;
+                    case 1:
+                        doReleaseNoInternet();
+                        break;
+                }
+
+            } else {
+                // Start and intent for the logged out activity
+                iUtil.goToActivity(new Intent(this, SignIn.class));
+            }
         }
+    }
+
+
+    private void doReleaseNoInternet() {
+
+    }
+
+    private void doDebug() {
+
     }
 }

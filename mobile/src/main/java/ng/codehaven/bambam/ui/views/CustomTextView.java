daa940dd618a147a514b573a138a9077aa2c3de2
Thompson Edolo <verygreenboi@live.com>
2015-08-08 23:17:08 +0100

    Implement custom fonts framework

---
+++ mobile/src/main/java/ng/codehaven/bambam/ui/views/CustomTextView.java
@@ -0,0 +1,24 @@
+package ng.codehaven.bambam.ui.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import ng.codehaven.bambam.utils.CustomFontHelper;
+
+
+public class CustomTextView extends TextView {
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        CustomFontHelper.setCustomFont(this, context, attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        CustomFontHelper.setCustomFont(this, context, attrs);
+    }
+}
